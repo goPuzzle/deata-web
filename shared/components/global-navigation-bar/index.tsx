7e2e69c4e5a83e1545fdaea1d9f4eee9da19e121
kimhecan <kimhecan@rainist.com>
2022-09-24 00:05:47 +0900

    test ci

--- shared/components/global-navigation-bar/index.tsx
+++ shared/components/global-navigation-bar/index.tsx
@@ -1,1 +1,10 @@
-export const GlobalNavigationBar = () => <div>GlobalNavigationBar</div>;
+import styled from '@emotion/styled';
+
+export const GlobalNavigationBar = () => <Container>GlobalNavigationBar</Container>;
+
+const Container = styled.header`
+  position: sticky;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
